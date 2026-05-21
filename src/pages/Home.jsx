e9398da2e19fd265215bb0760d1274f77291f6a6
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-21 11:26:33 +0530

    About section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 };
