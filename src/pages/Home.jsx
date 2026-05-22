b730102bbdb0cc3fc17c08c87f552562bca5cf1c
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-22 10:17:33 +0530

    Experience Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import About from "../components/About";
 import Skills from "../components/Skills";
 import Education from "../components/Education";
 import Projects from "../components/Projects";
+import Experience from "../components/Experience";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Skills />
       <Education />
       <Projects />
+      <Experience />
     </div>
   );
 };
